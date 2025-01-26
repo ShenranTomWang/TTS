75da499f16e36a5cb3ac317b09284accb576c6cb
shenranw <wsr.20021128@gmail.com>
2025-01-26 12:16:51 -0800

    remove files should be ignored

--- remove_npy.py
+++
@@ -1,8 +0,0 @@
-import os
-
-folder = "./synth_output-maliseet-matcha-hifigan"
-
-filelist = os.listdir(folder)
-for file in filelist:
-    if file.endswith(".npy"):
-        os.remove(f"{folder}/{file}")
